64b7313437b0cadce69678efdaacb5c03c912786
David Gumberg <davidzgumberg@gmail.com>
2024-09-10 00:14:33 -0700

    Don't buffer small AutoFile writes

--- src/streams.cpp
+++ src/streams.cpp
@@ -73,16 +73,30 @@ void AutoFile::write(Span<const std::byte> src)
     } else {
         auto current_pos{std::ftell(m_file)};
         if (current_pos < 0) throw std::ios_base::failure("AutoFile::write: ftell failed");
-        std::array<std::byte, 4096> buf;
-        while (src.size() > 0) {
-            auto buf_now{Span{buf}.first(std::min<size_t>(src.size(), buf.size()))};
+
+        // Don't buffer small writes
+        if (src.size() <= 4096) {
+            std::vector<std::byte> buf(src.begin(), src.end());
+
+            std::span<std::byte> buf_now{buf};
             std::copy(src.begin(), src.begin() + buf_now.size(), buf_now.begin());
             util::Xor(buf_now, m_xor, current_pos);
             if (std::fwrite(buf_now.data(), 1, buf_now.size(), m_file) != buf_now.size()) {
                 throw std::ios_base::failure{"XorFile::write: failed"};
             }
-            src = src.subspan(buf_now.size());
-            current_pos += buf_now.size();
+        }
+        else {
+            std::array<std::byte, 4096> buf;
+            while (src.size() > 0) {
+                auto buf_now{Span{buf}.first(std::min<size_t>(src.size(), buf.size()))};
+                std::copy(src.begin(), src.begin() + buf_now.size(), buf_now.begin());
+                util::Xor(buf_now, m_xor, current_pos);
+                if (std::fwrite(buf_now.data(), 1, buf_now.size(), m_file) != buf_now.size()) {
+                    throw std::ios_base::failure{"XorFile::write: failed"};
+                }
+                src = src.subspan(buf_now.size());
+                current_pos += buf_now.size();
+            }
         }
     }
 }
