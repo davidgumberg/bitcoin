627da6ab2ca98f195d2d333913e41bd73a3843d1
Pol Espinasa <pol.espinasa@uab.cat>
2025-09-20 00:59:10 +0200

    log: block reconstruction time

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -4327,6 +4327,7 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
             return;
         }
 
+        const auto time_start{SteadyClock::now()};
         CBlockHeaderAndShortTxIDs cmpctblock;
         vRecv >> cmpctblock;
 
@@ -4526,7 +4527,11 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
         if (fProcessBLOCKTXN) {
             BlockTransactions txn;
             txn.blockhash = blockhash;
-            return ProcessCompactBlockTxns(pfrom, *peer, txn);
+            ProcessCompactBlockTxns(pfrom, *peer, txn);
+            const auto elapsed_time = SteadyClock::now() - time_start;
+            LogDebug(BCLog::CMPCTBLOCK, "Optimistic reconstruction succeeded, taking: %.2fms\n",
+                Ticks<MillisecondsDouble>(elapsed_time));
+            return;
         }
 
         if (fRevertToHeaderProcessing) {
@@ -4563,6 +4568,9 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
                 // can't be used to interfere with block relay.
                 RemoveBlockRequest(pblock->GetHash(), std::nullopt);
             }
+            const auto elapsed_time = SteadyClock::now() - time_start;
+            LogDebug(BCLog::CMPCTBLOCK, "Optimistic reconstruction succeeded, taking: %.2fms\n",
+                Ticks<MillisecondsDouble>(elapsed_time));
         }
         return;
     }
