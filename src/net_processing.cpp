f44c7b9c7005e32bc32cec22a0b504abbe53b480
David Gumberg <davidzgumberg@gmail.com>
2026-07-15 22:45:11 -0400

    research: cmpctblocks: after IBD only request blocks from manual conns

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -6315,7 +6315,7 @@ bool PeerManagerImpl::SendMessages(CNode& node)
         // Message: getdata (blocks)
         //
         std::vector<CInv> vGetData;
-        if (CanServeBlocks(peer) && ((node.IsManualConn() && sync_blocks_and_headers_from_peer && !IsLimitedPeer(peer)) || !m_chainman.IsInitialBlockDownload()) && state.vBlocksInFlight.size() < MAX_BLOCKS_IN_TRANSIT_PER_PEER) {
+        if ((m_chainman.IsInitialBlockDownload() || node.IsManualConn()) && CanServeBlocks(peer) && ((sync_blocks_and_headers_from_peer && !IsLimitedPeer(peer)) || !m_chainman.IsInitialBlockDownload()) && state.vBlocksInFlight.size() < MAX_BLOCKS_IN_TRANSIT_PER_PEER) {
             std::vector<const CBlockIndex*> vToDownload;
             NodeId staller = -1;
             auto get_inflight_budget = [&state]() {
